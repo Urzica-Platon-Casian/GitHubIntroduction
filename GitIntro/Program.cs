7dfc56a90b22d46756a023d8c9258ee585f9581b
Casian Urzică-Platon <upcasian@yahoo.com>
2022-02-20 13:07:05 +0200

    Initialize Project

---
+++ GitIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
